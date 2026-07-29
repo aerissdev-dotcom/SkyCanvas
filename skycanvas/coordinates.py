80bc0bf7338825ca1ca988759725a86166b58bdf
Aeriss-Unity <unity.gamemaking.acc@gmail.com>
2026-07-29 10:29:10 +0200

    Added coordinates.py file, where the stars' coordinates are stored, deleted astronomy.py file

---
+++ skycanvas/coordinates.py
@@ -0,0 +1,271 @@
+COORDINATES = {
+     "orion": {
+        "Betelgeuse": (8, 3),
+        "Bellatrix": (22, 3),
+        "Alnitak": (11, 9),
+        "Alnilam": (15, 9),
+        "Mintaka": (19, 9),
+        "Saiph": (10, 16),
+        "Rigel": (22, 16)
+    },
+
+    "ursa_major": {
+        "Dubhe": (6, 4),
+        "Merak": (6, 11),
+        "Phecda": (15, 11),
+        "Megrez": (15, 5),
+        "Alioth": (24, 8),
+        "Mizar": (31, 8),
+        "Alkaid": (38, 8)
+    },
+
+    "cassiopeia": {
+        "Caph": (5, 6),
+        "Schedar": (12, 12),
+        "Gamma Cassiopeiae": (20, 5),
+        "Ruchbah": (28, 12),
+        "Segin": (36, 6)
+    },
+
+    "scorpius": {
+        "Dschubba": (18, 4),
+        "Graffias": (14, 6),
+        "Pi Scorpi": (12, 9),
+        "Antares": (16, 12),
+        "Sargas": (21, 15),
+        "Epsilon Scorpii": (25, 17),
+        "Shaula": (30, 18),
+        "Lesath": (33, 18)
+    },
+
+    "taurus": {
+        "Aldebaran": (20, 10),
+        "Theta Tauri": (15, 8),
+        "Ain": (12, 7),
+        "Elnath": (8, 3),
+        "Zeta Tauri": (27, 5),
+        "Atlas": (30, 12),
+        "Alcyone": (34, 12)
+    },
+
+    "gemini": {
+        "Castor": (14, 4),
+        "Pollux": (14, 9),
+        "Alhena": (22, 15),
+        "Wasat": (18, 10),
+        "Mebsuta": (21, 7),
+        "Tejat": (25, 12),
+        "Propus": (29, 15)
+    },
+
+    "leo": {
+        "Regulus": (10, 12),
+        "Algieba": (16, 8),
+        "Adhafera": (22, 6),
+        "Ras Elased Australis": (27, 4),
+        "Zosma": (24, 12),
+        "Chertan": (30, 14),
+        "Denebola": (36, 16)
+    },
+
+    "cygnus": {
+        "Deneb": (20, 3),
+        "Sadr": (20, 10),
+        "Gienah": (12, 16),
+        "Delta Cygni": (28, 16),
+        "Albireo": (20, 19)
+    },
+
+    "lyra": {
+        "Vega": (20, 4),
+        "Sheliak": (12, 12),
+        "Sulafat": (28, 12),
+        "Delta Lyrae": (15, 16),
+        "Epsilon Lyrae": (25, 16)
+    },
+
+    "aquila": {
+        "Tarazed": (12, 7),
+        "Altair": (20, 10),
+        "Alshain": (28, 13),
+        "Delta Aquilae": (20, 16),
+        "Lambda Aquilae": (14, 19)    
+    },
+
+    "pegasus": {
+        "Markab": (10, 12),
+        "Scheat": (10, 4),
+        "Alpheratz": (30, 4),
+        "Algenib": (30, 12),
+        "Enif": (18, 18),
+        "Homam": (24, 15),
+        "Matar": (28, 17)
+    },
+
+    "andromeda": {
+        "Alpheratz": (10, 5),
+        "Mirach": (18, 9),
+        "Almach": (30, 13),
+        "Delta Andromeda": (24, 6),
+        "Mu Andromeda": (30, 8),
+        "Upsilon Andromeda": (35, 10)
+    },
+
+    "canis_major": {
+        "Sirius": (18, 15),
+        "Mirzam": (12, 11),
+        "Wezen": (25, 10),
+        "Adhara": (30, 15),
+        "Aludra": (35, 18),
+        "Furud": (23, 18),
+        "Omicron² Canis Majoris": (38, 17)
+    },
+
+    "canis_minor": {
+        "Procyon": (15, 10),
+        "Gomeisa": (25, 7)
+    },
+
+    "draco": {
+        "Thuban": (12, 10),
+        "Rastaban": (18, 5),
+        "Eltanin": (25, 8),
+        "Grumium": (32, 12),
+        "Aldhibah": (30, 17),
+        "Edasich": (22, 18),
+        "Giausar": (15, 16)
+    },
+
+    "sagittarius": {
+        "Kaus Borealis": (12, 5),
+        "Kaus Media": (18, 9),
+        "Kaus Australis": (24, 14),
+        "Nunki": (30, 10),
+        "Ascella": (34, 15),
+        "Alnasl": (20, 18),
+        "Albaldah": (15, 20)
+    },
+
+    "virgo": {
+        "Vindemiatrix": (8, 5),
+        "Zavijava": (14, 8),
+        "Porrima": (20, 11),
+        "Spica": (28, 16),
+        "Syrma": (34, 18),
+        "Heze": (24, 8),
+        "Auva": (18, 5)
+    },
+
+    "aries": {
+        "Hamal": (12, 8),
+        "Sheratan": (20, 10),
+        "Mesarthim": (26, 12),
+        "Botein": (30, 6),
+        "41 Arietis": (34, 10)
+    },
+
+    "capricornus": {
+        "Algedi": (8, 8),
+        "Dabih": (14, 12),
+        "Nashira": (22, 8),
+        "Deneb Algedi": (30, 10),
+        "Omega Capricorni": (24, 16),
+        "Zeta Capricorni": (18, 18)
+    },
+
+    "pisces": {
+        "Alrescha": (20, 10),
+        "Eta Piscium": (10, 5),
+        "Gamma Piscium": (8, 14),
+        "Omega Piscium": (15, 18),
+        "Iota Piscium": (28, 14),
+        "Lambda Piscium": (35, 10)
+    },
+
+    "ursa_minor": {
+        "Polaris": (20, 3),
+        "Yildun": (20, 9),
+        "Zeta Ursae Minoris": (16, 14),
+        "Pherkad": (12, 17),
+        "Kochab": (28, 17),
+        "Epsilon Ursae Minoris": (24, 13),
+        "Eta Ursae Minoris": (8, 20)
+    },
+
+    "perseus": {
+        "Mirfak": (18, 8),
+        "Algol": (28, 12),
+        "Atik": (34, 8),
+        "Menkib": (38, 4),
+        "Misam": (12, 12),
+        "Rho Persei": (22, 16),
+        "Epsilon Persei": (30, 18)
+    },
+
+    "cepheus": {
+        "Alderamin": (20, 4),
+        "Alfirk": (12, 10),
+        "Errai": (28, 10),
+        "Zeta Cephei": (14, 17),
+        "Delta Cephei": (20, 14),
+        "Iota Cephei": (32, 18)
+    },
+
+    "hydra": {
+        "Alphard": (10, 10),
+        "Minchir": (16, 8),
+        "Gamma Hydrae": (22, 10),
+        "Zeta Hydrae": (28, 12),
+        "Ukdah": (34, 10),
+        "Ashlesha": (38, 8)
+    },
+
+    "centaurus": {
+        "Rigil Kentaurus": (14, 12),
+        "Hadar": (22, 10),
+        "Menkent": (10, 5),
+        "Muhlifain": (28, 15),
+        "Epsilon Centauri": (34, 12),
+        "Zeta Centauri": (38, 16)
+    },
+
+    "crux": {
+        "Gacrux": (20, 4),
+        "Mimosa": (20, 12),
+        "Acrux": (20, 20),
+        "Delta Crucis": (12, 12),
+        "Epsilon Crucis": (28, 12)
+    },
+
+    "phoenix": {
+        "Ankaa": (10, 10),
+        "Beta Phoenicis": (16, 6),
+        "Gamma Phoenicis": (22, 10),
+        "Zeta Phoenicis": (28, 14),
+        "Epsilon Phoenicis": (34, 10)
+    },
+
+    "libra": {
+        "Zubeneschamali": (14, 8),
+        "Zebenelgenubi": (22, 8),
+        "Brachium": (28, 14),
+        "Zubenelhakrabi": (34, 12)
+    },
+
+    "aquarius": {
+        "Sadalmelik": (14, 6),
+        "Sadalsuud": (22, 8),
+        "Skat": (30, 12),
+        "Albali": (18, 15),
+        "Ancha": (24, 18),
+        "Situla": (34, 17)
+    },
+
+    "cancer": {
+        "Altarf": (12, 12),
+        "Acubens": (20, 8),
+        "Asellus Borealis": (26, 5),
+        "Asellus Australis": (26, 15),
+        "Iota Cancri": (34, 10)
+    }
+}
